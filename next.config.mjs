eb08df6979e834818616cca2e87ede50f9a56e05
Zori <dev@zori.io>
2024-09-13 12:14:16 +0530

    next config is updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        source: '/api/(.*)', // Apply headers to all API routes
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://exodus-referral-admin.vercel.app' }, // Allow your domain
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,POST,PUT,DELETE' }, // Allow these HTTP methods
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' }, // Allow these headers
+        ],
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
